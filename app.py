42263522d13744c5bf2693244c8787cf02cc5f25
anjith <anjithpadmanabhan@gmil.com>
2022-04-29 04:12:23 +0530

    modification

--- app.py
+++ app.py
@@ -1,33 +1,27 @@
+#import frameworks and libreries
 from flask import Flask, render_template, request
 import requests
 import json
+#create flask app
 app = Flask(__name__)
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return render_template('index.html')
 
-    if request.method == "POST":
-        city = request.form['city']
-        api_key = "c048d46cbd48a21a01434f89363af3be"
+@app.route('/search', methods=['GET', 'POST'])
+def result():
 
-        weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city}&units=metric')
+    city = request.form['city']
+    api_key = "c048d46cbd48a21a01434f89363af3be"
 
-        weather_data = weather_url.json()
+    weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city}&units=metric')
 
-        out_file = open("myfile.json", "w") 
-    
-        json.dump(weather_data, out_file, indent = 6)
-        out_file.close()
+    weather_data = weather_url.json()
 
-        file_read = open("myfile.json", "r")
 
-        data = json.load(file_read)
-        file_read.close()
+    return render_template("result.html",weather = weather_data)
 
-        return render_template("result.html",weather = data)
-
-    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
